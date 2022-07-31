0ca371c647ede5b979921d43b6bc5d9274162beb
sherick14 <68566180+sherick14@users.noreply.github.com>
2022-07-30 21:53:30 -0600

    end the card activity

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -76,292 +76,143 @@
           </div>
         </div>
       </div>
-
-      <!-- <div class="md-layout">
-        <div class="md-layout-item">
-          <div class="image-wrapper">
-            <div class="row">
-              <div class="col-1"/>
-              <div class="col-6">
-                <h1>TODO LISTO PARA <br> NUTRIR BIEN TU CUERPO</h1>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-1"/>
-              <div class="col-6">
-                <p>Encuentra opciones deliciosas cercanas sin importar tu tipo de</p>
-              </div>
-            </div>
-
-            <div class="brand" style="text-align:left;">
-              <h1>TODO LISTO PARA NUTRIR BIEN TU CUERPO</h1>
-              <h3>Encuentra opciones deliciosas cercanas sin importar tu tipo de dieta.</h3>
-            </div>
-            <div class="row my-5 bg-white">
-              <div class="col-3">f</div>
-              <div class="col-3">f</div>
-              <div class="col-3">f</div>
-              
-            </div>
-          </div>
-        </div>
-      </div> -->
     </parallax>
 
     <div class="main main-raised">
-      <div class="section section-basic">
+      <div class="section section-basic" style="background: #f5f5f5">
         <div class="container">
-          <div class="title">
-            <h2>Proveedores Greenies</h2>
+          <div class="title center">
+            <h2 class="mainTitle">Proveedores Greenies</h2>
             <br />
-            <label for=""> Conoce los proveedores más solicitados</label>
-          </div>
-          <basic-elements></basic-elements>
-          <a href="#">ver más ></a>
-        </div>
-      </div>
-
-      <div class="section section-tabs">
-        <div class="container">
-          <tabs></tabs>
-        </div>
-      </div>
-      <div class="section section-white">
-        <div class="container">
-          <nav-pills></nav-pills>
-        </div>
-      </div>
-      <div class="section section-notifications">
-        <div class="container">
-          <div class="title">
-            <h3>Notifications</h3>
-          </div>
-        </div>
-        <notifications></notifications>
-      </div>
-      <div class="section">
-        <div class="container">
-          <typography-images></typography-images>
-        </div>
-      </div>
-      <div class="section section-javascript">
-        <div class="container">
-          <javascript-components></javascript-components>
-        </div>
-      </div>
-      <div class="section">
-        <div class="container text-center">
-          <div class="md-layout">
-            <div
-              class="
-                md-layout-item md-size-66 md-xsmall-size-100
-                ml-auto
-                mr-auto
-                text-center
-              "
+            <label for="" class="subtitleText">
+              Conoce los proveedores más solicitados</label
             >
-              <h2>Completed with examples</h2>
-              <h4>
-                The kit comes with three pre-built pages to help you get started
-                faster. You can change the text and images and you're good to
-                go. More importantly, looking at them will give you a picture of
-                what you can built with this powerful kit.
-              </h4>
-            </div>
           </div>
-        </div>
-      </div>
-      <div class="section section-signup page-header" :style="signupImage">
-        <div class="container">
-          <div class="md-layout">
-            <div
-              class="
-                md-layout-item
-                md-size-33
-                md-medium-size-40
-                md-small-size-50
-                md-xsmall-size-70
-                mx-auto
-                text-center
-              "
-            >
-              <login-card header-color="green">
-                <h4 slot="title" class="card-title">Login</h4>
-                <md-button
-                  slot="buttons"
-                  href="javascript:void(0)"
-                  class="md-just-icon md-simple md-white"
-                >
-                  <i class="fab fa-facebook-square"></i>
-                </md-button>
-                <md-button
-                  slot="buttons"
-                  href="javascript:void(0)"
-                  class="md-just-icon md-simple md-white"
-                >
-                  <i class="fab fa-twitter"></i>
-                </md-button>
-                <md-button
-                  slot="buttons"
-                  href="javascript:void(0)"
-                  class="md-just-icon md-simple md-white"
-                >
-                  <i class="fab fa-google-plus-g"></i>
-                </md-button>
-                <p slot="description" class="description">Or Be Classical</p>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>face</md-icon>
-                  <label>First Name...</label>
-                  <md-input v-model="firstname"></md-input>
-                </md-field>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>email</md-icon>
-                  <label>Email...</label>
-                  <md-input v-model="email" type="email"></md-input>
-                </md-field>
-                <md-field class="md-form-group" slot="inputs">
-                  <md-icon>lock_outline</md-icon>
-                  <label>Password...</label>
-                  <md-input v-model="password"></md-input>
-                </md-field>
-                <md-button slot="footer" class="md-simple md-success md-lg">
-                  Get Started
-                </md-button>
-              </login-card>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="md-layout">
-        <div class="md-layout-item text-center">
-          <md-button href="#/login" class="md-simple md-success md-lg"
-            >View Login Page</md-button
-          >
-        </div>
-      </div>
-      <div class="section section-examples">
-        <div class="container-fluid text-center">
-          <div class="md-layout">
-            <div class="md-layout-item">
-              <a href="#/landing" target="_blank">
-                <img
-                  :src="landing"
-                  alt="Rounded Image"
-                  class="img-raised rounded img-fluid"
-                />
-              </a>
-              <md-button href="#/landing" class="md-simple md-success md-lg"
-                >View Landing Page</md-button
-              >
-            </div>
-            <div class="md-layout-item">
-              <a href="#/profile" target="_blank">
-                <img
-                  :src="profile"
-                  alt="Rounded Image"
-                  class="img-raised rounded img-fluid"
-                />
-              </a>
-              <md-button href="#/profile" class="md-simple md-success md-lg"
-                >View Profile Page</md-button
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="section section-download" id="downloadSection">
-        <div class="container">
-          <div class="md-layout text-center">
-            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
-              <h2>Do you love this UI Kit?</h2>
-              <h4>
-                Cause if you do, it can be yours for FREE. Hit the buttons below
-                to navigate to our website where you can find the kit. Our
-                friends from
-                <a
-                  href="https://themeisle.com/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
-                  target="_blank"
-                  >ThemeIsle</a
-                >
-                created a Wordpress Theme which can be also downloaded for free.
-                Start a new project or give an old Bootstrap project a new look!
-              </h4>
-            </div>
-            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
-              <md-button
-                href="https://www.creative-tim.com/product/material-kit"
-                target="_blank"
-                class="md-success md-lg"
-                ><i class="fab fa-html5"></i> Free HTML Download</md-button
-              >
-              <md-button
-                href="https://themeisle.com/themes/hestia/?utm_campaign=mkfree-hestia&amp;utm_source=creativetim&amp;utm_medium=website"
-                class="md-success md-lg"
-                target="_blank"
-                ><i class="fab fa-wordpress"></i> Wordpress Theme</md-button
-              >
-            </div>
-          </div>
-          <br /><br />
 
-          <div class="md-layout text-center">
-            <div class="md-layout-item md-size-66 md-small-size-100 mx-auto">
-              <h2>Want more?</h2>
-              <h4>
-                We've just launched
-                <a
-                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-mk-demos"
-                  target="_blank"
-                  >Vue Material Dashboard PRO</a
+          <div class="row">
+            <!-- card 1 -->
+            <div class="col-3">
+              <div class="card">
+                <img
+                  src="@/assets/img/Frenchtoast.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div
+                  class="card-body"
+                  style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
                 >
-                and
-                <a
-                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
-                  target="_blank"
-                  >Vue Material Kit PRO</a
-                >. Every product has a huge number of components, sections and
-                example pages. Start Your Development With A Badass Bootstrap UI
-                Kit inspired by Material Design.
-              </h4>
+                  <p style="margin: 0px 0px 0px 0px" class="cardTitle mt-2">
+                    Oui Oui Breakfast
+                  </p>
+                  <p style="margin: 0px 0px 0px 0px" class="cardSubtitle mt-1">
+                    Juárez, Chihuahua
+                  </p>
+                  <div class="space-50">
+                    <img src="@/assets/img/Estrella.svg" />
+                    <img src="@/assets/img/Estrella.svg" class="px-1" />
+                    <img src="@/assets/img/Estrella.svg" />
+                    <label for="" class="cardReviewText">(3 reseñas)</label>
+                  </div>
+                  <div class="mt-2">
+                    <a href="#" class="cardShowMore" style="color: #000">Ver más ></a>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="md-layout-item md-size-50 md-small-size-100 mx-auto">
-              <div class="btn-container">
-                <md-button
-                  href="https://www.creative-tim.com/product/vue-material-kit-pro?ref=utp-vmk-demos"
-                  class="md-button md-success md-lg md-upgrade"
-                  target="_blank"
-                  ><md-icon>unarchive</md-icon>Vue Material Kit PRO</md-button
+            <!-- card 2 -->
+            <div class="col-3">
+              <div class="card">
+                <img
+                  src="@/assets/img/Donas.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div
+                  class="card-body"
+                  style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
                 >
-                <md-button
-                  href="https://www.creative-tim.com/product/vue-material-dashboard-pro?ref=utp-vmd-demos"
-                  class="md-button md-success md-lg md-upgrade"
-                  target="_blank"
-                  ><md-icon>unarchive</md-icon>Vue Material Dashboard
-                  PRO</md-button
+                  <p style="margin: 0px 0px 0px 0px" class="cardTitle mt-2">
+                    La Dona Keto
+                  </p>
+                  <p style="margin: 0px 0px 0px 0px" class="cardSubtitle mt-1">
+                    Chihuahua, Chihuahua
+                  </p>
+                  <div class="space-50">
+                    <img src="@/assets/img/Estrella.svg" />
+                    <img src="@/assets/img/Estrella.svg" class="px-1" />
+                    <img src="@/assets/img/Estrella.svg" />
+                    <label for="" class="cardReviewText">(10 reseñas)</label>
+                  </div>
+                  <div class="mt-2">
+                    <a href="#" class="cardShowMore" style="color: #000">Ver más ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- card 3 -->
+            <div class="col-3">
+              <div class="card">
+                <img
+                  src="@/assets/img/Paletas.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div
+                  class="card-body"
+                  style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
                 >
+                  <p style="margin: 0px 0px 0px 0px" class="cardTitle mt-2">
+                    Green Pop
+                  </p>
+                  <p style="margin: 0px 0px 0px 0px" class="cardSubtitle mt-1">
+                    Delicias, Chihuahua
+                  </p>
+                  <div class="space-50">
+                    <img src="@/assets/img/Estrella.svg" />
+                    <img src="@/assets/img/Estrella.svg" class="px-1" />
+                    <img src="@/assets/img/Estrella.svg" />
+                    <img src="@/assets/img/Estrella.svg" class="px-1"/>
+                    <label for="" class="cardReviewText">(10 reseñas)</label>
+                  </div>
+                  <div class="mt-2">
+                    <a href="#" class="cardShowMore" style="color: #000">Ver más ></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- card 4 -->
+            <div class="col-3">
+              <div class="card">
+                <img
+                  src="@/assets/img/Avotoast.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div
+                  class="card-body"
+                  style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
+                >
+                  <p style="margin: 0px 0px 0px 0px" class="cardTitle mt-2">
+                    Avo Place
+                  </p>
+                  <p style="margin: 0px 0px 0px 0px" class="cardSubtitle mt-1">
+                    Chihuahua, Chihuahua
+                  </p>
+                  <div class="space-50">
+                    <img src="@/assets/img/Estrella.svg" />
+                    <img src="@/assets/img/Estrella.svg" class="px-1" />
+                    <label for="" class="cardReviewText">(12 reseñas)</label>
+                  </div>
+                  <div class="mt-2">
+                    <a href="#" class="cardShowMore" style="color: #000">Ver más ></a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-
-          <div class="sharing-area text-center">
-            <div class="md-layout">
-              <div class="md-layout-item">
-                <h3>Thank you for supporting us!</h3>
-              </div>
-            </div>
-
-            <md-button class="md-twitter"
-              ><i class="fab fa-twitter"></i>Tweet</md-button
-            >
-            <md-button class="md-facebook"
-              ><i class="fab fa-facebook-square"></i> Share</md-button
-            >
-            <md-button class="md-google"
-              ><i class="fab fa-google-plus"></i> Share</md-button
-            >
-            <md-button class="md-github"
-              ><i class="fab fa-github"></i> Star</md-button
-            >
+          <div class="center mt-5 subtitleText">
+            <a href="#" style="color: #000">Ver más ></a>
           </div>
         </div>
       </div>
@@ -448,17 +299,19 @@
         <div>
           <b-jumbotron class="jumbotronImage page-header" :style="jumboStyle">
             <div class="container">
-              <div class="space-100"/>
-              <div class="space-30"/>
+              <div class="space-100" />
+              <div class="space-30" />
               <h2 class="jumbotronTitle">Únete a la red de Mr. Greenie</h2>
-              <div class="space-70"/>
+              <div class="space-70" />
               <div class="row">
                 <div class="col-6">
                   <p class="jumboText700">
                     Se parte del directorio de Mr. Greenie y deja que más
                     comensales lleguen a ti.
                   </p>
-                  <p class="jumboText400 mt-5">Estar registrado en nuestra red es gratuito.</p>
+                  <p class="jumboText400 mt-5">
+                    Estar registrado en nuestra red es gratuito.
+                  </p>
                   <div class="space-50"></div>
                   <div class="center">
                     <button class="jumboButton">Unirme</button>
@@ -466,7 +319,11 @@
                 </div>
                 <div class="col-6 center">
                   <div class="space-30"></div>
-                  <img src="@/assets/img/Greenie_grande.png" fluid alt="Fluid image" />
+                  <img
+                    src="@/assets/img/Greenie_grande.png"
+                    fluid
+                    alt="Fluid image"
+                  />
                 </div>
               </div>
             </div>
@@ -491,13 +348,8 @@
 </template>
 
 <script>
-import BasicElements from "./components/BasicElementsSection";
 import SmallNavigation from "./components/SmallNavigationSection";
-import Tabs from "./components/TabsSection";
-import NavPills from "./components/NavPillsSection";
-import Notifications from "./components/NotificationsSection";
 import TypographyImages from "./components/TypographyImagesSection";
-import JavascriptComponents from "./components/JavascriptComponentsSection";
 import { LoginCard } from "@/components";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
@@ -506,14 +358,8 @@ Vue.use(BootstrapVue);
 
 export default {
   components: {
-    BasicElements,
     SmallNavigation,
-    Tabs,
-    NavPills,
-    Notifications,
-    TypographyImages,
-    JavascriptComponents,
-    LoginCard,
+    //LoginCard,
   },
   name: "index",
   bodyClass: "index-page",
